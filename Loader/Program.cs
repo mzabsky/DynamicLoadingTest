d9348dcfcd1679e7a8c6dda5e88112f5c9b6593c
Matěj Zábský <mzabsky@gmail.com>
2020-05-21 16:50:02 +0200

    Initial commit

---
+++ Loader/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+
+namespace Loader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var assembly = Assembly.LoadFrom("../../../../DynamicLoadingTest/bin/Debug/netcoreapp3.1/DynamicLoadingTest.dll");
+            var references = assembly.GetExportedTypes();
+            var type = assembly.GetType("Program");
+
+        }
+    }
+}
